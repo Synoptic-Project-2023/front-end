a0db0c4711c0c4e6648bd2c6c8882327b8663550
Niisoks <colebrhall@gmail.com>
2023-06-12 18:29:42 +0100

    included I18n provider over main nav and translated page titles

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './localisation/translations';
 
 import MapScreen from './pages/MapScreen';
 import Login from './pages/Login';
@@ -16,61 +18,63 @@ const Stack = createNativeStackNavigator();
 
 export default function App({ navigation }) {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
+        <I18nextProvider i18n={i18n}>
+            <NavigationContainer>
+                <Stack.Navigator>
 
-                <Stack.Screen
-                    name="Map"
-                    component={MapScreen}
-                    options={{
-                        title: 'Map',
-                        headerShown: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{
-                        title: 'Login',
-                    }}
-                />
-                <Stack.Screen
-                    name="Profile"
-                    component={Profile}
-                    options={{
-                        title: 'Profile',
-                    }}
-                />
-                <Stack.Screen
-                    name="Request"
-                    component={Request}
-                    options={{
-                        title: 'Request Volunteer Access',
-                    }}
-                />
-                <Stack.Screen
-                    name="UpdateBank"
-                    component={UpdateBank}
-                    options={{
-                        title: 'Update Bank Inventory',
-                    }}
-                />
-                <Stack.Screen
-                    name="Inbox"
-                    component={Messages}
-                    options={{
-                        title: 'Inbox',
-                    }}
-                />
-                <Stack.Screen
-                    name="Register"
-                    component={Register}
-                    options={{
-                        title: 'Register',
-                    }}
-                />
+                    <Stack.Screen
+                        name="Map"
+                        component={MapScreen}
+                        options={{
+                            title: i18n.t('map'),
+                            headerShown: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                        options={{
+                            title: i18n.t('login'),
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Profile"
+                        component={Profile}
+                        options={{
+                            title: i18n.t('profil'),
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Request"
+                        component={Request}
+                        options={{
+                            title: i18n.t('request volunteer access'),
+                        }}
+                    />
+                    <Stack.Screen
+                        name="UpdateBank"
+                        component={UpdateBank}
+                        options={{
+                            title: i18n.t('update bank inventory'),
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Inbox"
+                        component={Messages}
+                        options={{
+                            title: i18n.t('inbox'),
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Register"
+                        component={Register}
+                        options={{
+                            title: i18n.t('register'),
+                        }}
+                    />
 
-            </Stack.Navigator>
-        </NavigationContainer>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </I18nextProvider>
     );
 };
