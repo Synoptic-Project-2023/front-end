06b8273e72fc1167050d711e09d8d08d570b52b1
Niisoks <colebrhall@gmail.com>
2023-06-12 18:29:16 +0100

    updated localisation and added to package.json

--- localisation/translations.js
+++ localisation/translations.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
-import es from ''
+import es from './es.json'
 
 i18n.use(initReactI18next).init({
     resources: {
@@ -12,7 +12,7 @@ i18n.use(initReactI18next).init({
             translation: es
         }
     },
-    lng: 'en',
+    lng: 'es',
     interpolation: {
         escapeValue: false
     },
